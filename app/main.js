3285d6a08df7f0d047c5b8156dcd05d4f72b0598
atahami3 <89944617+atahami3@users.noreply.github.com>
2022-11-29 11:45:37 -0800

    Update main.js

---
+++ app/main.js
@@ -0,0 +1,11 @@
+
+function computerChoice(){
+    const choice = ["Rock", "Paper", "Scissors"]
+    const randomChoice = Math.floor(Math.random() * 3)
+    choice[randomChoice]
+    return choice[randomChoice]
+}
+
+function getResults(){
+
+}
