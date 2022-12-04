f456d5aa7b9b9ddd18d79cd2ab04fc8a2b69195b
dharbo <72952082+dharbo@users.noreply.github.com>
2022-12-03 17:30:43 -0800

    Delete app, since we're going to merge gh-pages branch into main

--- app/modules/getComputerSelection.js
+++
@@ -1,6 +0,0 @@
-// get the computer's selection randomly between the options: 'Rock', 'Paper', or 'Scissors'
-export function getComputerSelection () {
-  const choice = ['Rock', 'Paper', 'Scissors']
-  const randomChoice = Math.floor(Math.random() * 3)
-  return choice[randomChoice]
-}
--- app/modules/userSelection.js
+++
@@ -1,9 +0,0 @@
-// get the user's selection, either 'Rock', 'Paper', or 'Scissors'
-export function getUserSelection (option) {
-  if (option.dataset.selection === 'Rock') {
-    return 'Rock'
-  } else if (option.dataset.selection === 'Paper') {
-    return 'Paper'
-  }
-  return 'Scissors'
-}
